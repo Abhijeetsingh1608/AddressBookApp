6b4a59fdaca80db4505c7ba118e385b2b8f88c09
Abhijeetsingh1608 <kumarabhisingh2020@gmail.com>
2026-03-08 18:25:33 +0530

    [Abhijeet]: UC3 - Implemented edit contact functionality with service layer, PUT API and edge case tests

--- src/test/java/com/addressbook/AddressBookServiceTest.java
+++ src/test/java/com/addressbook/AddressBookServiceTest.java
@@ -135,7 +135,7 @@ public class AddressBookServiceTest {
         AddressBookService service = new AddressBookService();
 
         Contact contact = new Contact(
-                "Tarus",
+                "Abhijeet",
                 "Prabhat",
                 "New City",
                 "Ariana",
@@ -189,5 +189,79 @@ public class AddressBookServiceTest {
 
         assertEquals(1,
                 service.getAddressBook("friends").getContacts().size());
+    }
+    
+    @Test
+    public void givenExistingContact_whenUpdated_shouldReturnUpdatedContact() {
+
+        AddressBookService service = new AddressBookService();
+
+        Contact original = new Contact(
+                "Abhijeet",
+                "Prabhat",
+                "New City",
+                "Ariana",
+                "Geornite",
+                "567834",
+                "7634237809",
+                "tp@gmail.com"
+        );
+
+        service.addContact("personal", original);
+
+        Contact updated = new Contact(
+                "Abhijeet",
+                "Prabhat",
+                "Future City",
+                "Nova",
+                "Geornite",
+                "567834",
+                "9999999999",
+                "tarus@update.com"
+        );
+
+        Contact result = service.updateContact(
+                "personal",
+                "Abhijeet",
+                "Prabhat",
+                updated
+        );
+
+        assertEquals("Nova", result.getCity());
+        assertEquals("9999999999", result.getPhoneNumber());
+    }
+    
+    @Test
+    public void givenNonExistingContact_whenUpdate_shouldReturnNull() {
+
+        AddressBookService service = new AddressBookService();
+
+        Contact updated = new Contact();
+
+        Contact result = service.updateContact(
+                "personal",
+                "Unknown",
+                "Person",
+                updated
+        );
+
+        assertNull(result);
+    }
+    
+    @Test
+    public void givenMissingAddressBook_whenUpdate_shouldReturnNull() {
+
+        AddressBookService service = new AddressBookService();
+
+        Contact updated = new Contact();
+
+        Contact result = service.updateContact(
+                "unknownBook",
+                "Abhijeet",
+                "Prabhat",
+                updated
+        );
+
+        assertNull(result);
     }
 }
--- src/test/java/com/addressbook/ContactTest.java
+++ src/test/java/com/addressbook/ContactTest.java
@@ -3,8 +3,6 @@ package com.addressbook;
 import com.addressbook.model.Contact;
 import org.junit.jupiter.api.Test;
 
-import com.addressbook.Contact;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ContactTest {
